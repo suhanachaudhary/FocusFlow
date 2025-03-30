95fea5414244f27d634b54e30da78273cb3b9923
upagyasingh <upagyasinght48@gmail.com>
2025-03-30 12:13:25 +0530

    removed backend

--- backend/src/schemas/deckSchema.js
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const deckSchema = z.object({
-    name: z.string().min(1, "name  is required"),
-    description: z.string().optional(),
-    userId: z.string().min(1, "User ID is required"),
-});
